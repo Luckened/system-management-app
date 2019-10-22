eae967ecb1d01630109775ac07bc7e91639fe4eb
Sávio Cabral <saviocampolina9@gmail.com>
2019-10-22 16:54:11 -0300

    Modifica usuarios

--- bash_interface/Main.java
+++ bash_interface/Main.java
@@ -1,24 +1,33 @@
 package bash_interface;
 
 import java.util.*;
+
 /**
  * main
  */
 public class Main {
-  public static void main(String [] args) {
+  public static void main(String[] args) {
     Scanner read = new Scanner(System.in);
     System.out.println("Bem-vindo!! O que deseja fazer?");
-    System.out.println("1 - Login");
-    System.out.println("2 - Cadastrar");
+    // System.out.println("1 - Login");
+    System.out.println("1 - Cadastrar");
+    System.out.println("2 - Editar");
+    System.out.println("3 - Sair");
     int resp = read.nextInt();
 
-    switch(resp){
-      case 1: 
-      break;
+    while (resp != 3) {
+
+      switch (resp) {
+      case 1:
+
+        break;
       case 2:
-      break;
+        break;
       default:
-        
+
+      }
+
+      System.out.println("Algo mais?");
     }
   }
 }
